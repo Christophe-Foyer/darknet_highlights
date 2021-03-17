6c7cdeab0acebd65e64db8bea9743ffb5b3f44d2
Christophe Foyer <christophe.aa.foyer@gmail.com>
2021-03-17 15:42:29 +0000

    added tests

--- tests/test_data/test_post_processing/test_post_processing.py
+++ tests/test_data/test_post_processing/test_post_processing.py
@@ -5,12 +5,12 @@ from darknet_highlights.data.post_process import Maui63DataProcessor
 
 
 
-logs = Path(__file__).parent / '../test_logs/Drone_Flight_Path_Dummy_Data.csv'
+logs = Path(__file__).parent / 'Drone_Flight_Path_Dummy_Data.csv'
 
 # Testfiles
 video = Path(__file__).parent / 'Maui Dolphins Aerial Footage - 2017.mp4'
 # image = Path(__file__).parent / 'maui-dolphins-the-gaps-boren-1200x783.jpg'
-image = Path(__file__).parent / 'Maui Dolphins Aerial Footage - 2017.jpg'
+image = Path(__file__).parent / 'Maui Dolphins Aerial Footage - 2017.png'
 
 # These files not on github repo, please provide your own
 data_file = Path(__file__).parent / '../../../../maui_sf_and_100m.data'
@@ -47,7 +47,7 @@ def test_videoprocessing_detection():
     
     # 50 is arbitrary, but should work for most decent CV code (currently 86)
     assert len(processor.dnn_df) > 50, \
-    'Dolphins should be detected in most of the frames'
+        'Dolphins should be detected in most of the frames'
     
 def test_imageprocessing_detection():
     processor = Maui63DataProcessor(
@@ -58,14 +58,50 @@ def test_imageprocessing_detection():
         weights, 
         names_file,
         highlighter_kwargs = highlighter_kwargs,
-        output_path = 'test.jpg'
+        output_path = '__temp__.jpg'
         )
     
     processor._run_cv()
     
     # 50 is arbitrary, but should work for most decent CV code (currently 86)
     assert processor.dnn_df.num_objects.to_numpy()[0] >= 1, \
-    'Dolphins should be detected in the test image'
+        'Dolphins should be detected in the test image'
+    
+def test_videoprocessing_process():
+    processor = Maui63DataProcessor(
+        logs,
+        video, 
+        data_file, 
+        config_file, 
+        weights, 
+        names_file,
+        highlighter_kwargs = highlighter_kwargs,
+        output_path = '__temp__'
+        )
+    
+    processor.process()
+    
+    # 50 is arbitrary, but should work for most decent CV code (currently 86)
+    assert len(processor.data) > 50, \
+        'Dolphins should be detected in most of the frames'
+        
+def test_imageprocessing_process():
+    processor = Maui63DataProcessor(
+        logs,
+        image, 
+        data_file, 
+        config_file, 
+        weights, 
+        names_file,
+        highlighter_kwargs = highlighter_kwargs,
+        output_path = '__temp__.jpg'
+        )
+    
+    processor.process()
+    
+    assert processor.dnn_df.num_objects.to_numpy()[0] >= 1, \
+        'Dolphins should be detected in the test image'
+    
 
 if __name__ == '__main__':
     pytest.main()
